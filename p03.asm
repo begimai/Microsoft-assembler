172964a129a6ef57448b93446d8803571e890fbb
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-03-13 23:20:17 +0600

    incrementing a letter
    all chars have their own ascii codes, which are consequen. Lowercase 'a'
    equals to 97, so 'b' is 98 and so on

---
+++ p03.asm
@@ -0,0 +1,22 @@
+.386  ; type of processor
+
+.model flat, stdcall ; for windows - flat
+
+include kernel32.inc  ; for exit
+includelib kernel32.lib ; where libs are
+
+.data
+	letter    byte    'a'
+	
+.code
+start:
+	inc letter
+	
+	push 0
+	call ExitProcess
+end start
+
+; to compile    ml /c /coff p00.asm
+; to link 		link /subsystem:console p00.obj
+; to debug 		ollydbg p00   + f9
+; EIP - extended instruction Pointer 
