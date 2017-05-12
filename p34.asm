fa336dad463858ac46cbae13926b9c373ca9d7c4
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-05-12 08:58:19 +0600

    Update p34.asm

--- p34.asm
+++ p34.asm
@@ -1,57 +1,60 @@
-.386  
+.686 
 
 .model flat, stdcall 
 
-include kernel32.inc  
-includelib kernel32.lib 
+include kernel32.inc
+includelib kernel32.lib
 includelib msvcrt.lib
 
 printf proto c
 
-.data
-	strFrmt		byte	"%d", 13, 10, 0
-	a			dword 	9, -1, 7, 0, 10
+.data 
+
+	frmtStr			byte	"%d", 13, 10, 0
+	a				dword	4, 1, 3, 2, -10, 12
+	
 	
 .code
 start:
 	push offset cmpInts
-	push 5							; number of elements
-	push 4							; size of integer
+	push 6
+	push 4
 	push offset a
-	call minElement
+	call minElem
 	
 	push [eax]
-	push offset strFrmt
+	push offset frmtStr
 	call printf
 	
 	add esp, 24
 	
-	push 0
-	call ExitProcess
-	
-minElement proc
-	push esi						; address of current min
-	push edi 						; address of current element
-	push ebx						; adress of cmp function
+	push 0 
+	call ExitProcess 
+
+cmpInts proc
+	mov ecx, [esp + 4]
+	mov edx, [esp + 8]
+	mov eax, [ecx]
+	sub eax, [edx]
+	ret
+cmpInts endp
+
+minElem proc
+	push esi ;adress of current min 
+	push edi ;adress of current element
+	push ebx ;adress of cmp function
 	
 	mov ebx, [esp + 28]
-	; ebx now has the address of the comparison function
-	mov eax, [esp + 20]				;elemSize
-	; eax - size of integer = 4
-	mov esi, [esp + 16]				;pMin 
-	; esi now is the address of the first element of array
-	mov edi, esi					;pCur
+	mov edx, [esp + 24] ;num of elements in array
+	mov esi, [esp + 16]
+	mov eax, [esp + 20] ; eax - num of bytes in one element
+	mov edi, esi 
 	add edi, eax
-	; edi is the element after esi
-	
-	mov ecx, 1						;1
-	; ecx - variable i
-	mov edx, [esp + 24]				;len
-	; edx - number of elements in array
-	
-lblFor:
+	mov ecx, 1 ;is variable i
+
+labFor:
 	cmp ecx, edx
-	jge lblEnd
+	jge labEnd
 	
 	push eax
 	push ecx
@@ -60,39 +63,25 @@ lblFor:
 	push esi
 	push edi
 	call ebx
-	; call comparison function, the result is stored in eax
-	add esp, 8 	
+	add esp, 8
+	cmp eax, 0
+	jge labNext
 	
+	mov esi, edi
+	
+labNext:	
 	pop edx
 	pop ecx
 	pop eax
-
-	cmp eax, 0
-	jge lblNext
-	;else
-	mov esi, edi
 	
-lblNext:
-	
-	inc ecx
 	add edi, eax
-	jmp lblFor
-	
-lblEnd:
+	inc ecx
+	jmp labFor
+labEnd:	
 	mov eax, esi
-	;min stored in EAX after function call
 	pop ebx
 	pop edi
 	pop esi
 	ret
-minElement endp
-
-cmpInts proc
-	mov ecx, [esp + 4] ;pCur
-	mov edx, [esp + 8] ;pMin
-	mov eax, [ecx]
-	sub eax, [edx]
-	; the result of comparison is stored in eax
-	ret
-cmpInts endp
+minElem endp
 end start
